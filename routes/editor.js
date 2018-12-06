3bd9a3ab77f4cfa1c4bcd98860b614f82218223e
switch <shg18.sk1.hr10@gmail.com>
2018-12-06 11:06:21 +0900

    こちらリポジトリに移行する

---
+++ routes/editor.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+
+router.get('/', (req, res, next) => {
+  res.render('editor', {title: 'Latex Editor'});
+});
+
+router.post('/', (req, res, next) => {
+  jsonData = JSON.stringify(req.body.msg).slice(1, -1);
+  //jsonを受け取り、文頭・文末の「"」を削除する
+  var arr = jsonData.split(/\\n/);
+  //var arr = jsonData.split(/(?!\\\\newpage)(?=\\n)/);
+  // \newpageの処理は保留
+  for (var i = 0; i < arr.length; i++){
+    //console.log(arr[i]);
+    arr[i] = arr[i].replace(/\\\\/g, '\\');
+    fs.appendFileSync('sample.tex', arr[i] + '\n', function (err) {
+      console.log(err);
+    });
+  }
+  res.send(jsonData);
+});
+
+module.exports = router;
